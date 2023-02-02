ef4b3554ccaf097de2403d97b28c3d6348050d7b
zhenyulei <zhenyulei@jd.com>
2023-02-02 11:31:58 +0800

    feat: 初始化coupon组件

--- src/packages/coupon/coupon.tsx
+++ src/packages/coupon/coupon.tsx
@@ -1,31 +1,102 @@
-import React, {
-  FunctionComponent,
-} from 'react'
-import { useConfig } from '@/packages/configprovider'
+import React, { FunctionComponent, useEffect, CSSProperties } from "react";
+import { useConfig } from "@/packages/configprovider";
+import { IComponent } from "@/utils/typings";
+import bem from "@/utils/bem";
+import "./coupon.scss";
 
-import { IComponent } from '@/utils/typings'
-
-export interface CouponProps extends IComponent {
+interface ICouponType {
+  price: number; //优惠券价格或者折扣价
+  currency: string; //货币符号
+  mainTitle: string; //主标题
+  subTitle: string; //副标题
+  timeRange: string; //优惠券使用时间范围
+  label: string; //优惠券左上角的标签内容
 }
-
+export interface CouponProps extends IComponent {
+  columns: number; //每行有几个
+  couponWidth: string; //每张优惠券的宽度 30px 或者 30%
+  couponHeight: string; //每张优惠券的高度
+  couponRightWidth: string; //优惠券右侧的宽度10px或者10%
+  couponBgImg: string; //优惠券的背景图
+  pricePosition: "front" | "back"; //价格和标签的前后位置
+  coupon: ICouponType; //渲染优惠券内容
+  btnText: string; //按钮文案
+  btnStryle: CSSProperties; //按钮样式
+  isReceived: boolean; //是否领取
+  receivedBtnText: string; //领取后的按钮文案
+  receivedBtnStryle: CSSProperties; //领取后的按钮样式
+  className: string;
+}
+const couponObj = {
+  price: 9,
+  currency: "$",
+  mainTitle: "满100元可用",
+  subTitle: "仅可购买满折券测试商品",
+  timeRange: "2022.03.01-2022.04.01",
+  label: "内购专享",
+} as ICouponType;
 const defaultProps = {
-} as CouponProps
+  columns: 1,
+  couponWidth: "100%",
+  couponHeight: "90px",
+  couponRightWidth: "110px",
+  couponBgImg:
+    "https://storage.360buyimg.com/jdcdkh/open/1.0.0/assets/bg-coupon-red.f6ae2e19.png",
+  pricePosition: "front",
+  coupon: couponObj,
+  btnText: "立即领取",
+  btnStryle: {
+    color: "#fff",
+    backgroundColor: "red",
+  },
+  isReceived: false,
+  receivedBtnText: "已经领取",
+  receivedBtnStryle: {
+    color: "red",
+    backgroundColor: "#fff",
+  },
+} as CouponProps;
 
 export const Coupon: FunctionComponent<
-  Partial<CouponProps> & Omit<React.HTMLAttributes<HTMLDivElement>, 'onChange'>
+  Partial<CouponProps> & Omit<React.HTMLAttributes<HTMLDivElement>, "onChange">
 > = (props) => {
-  const { locale } = useConfig()
+  const { locale } = useConfig();
   const {
+    columns,
+    couponWidth,
+    couponHeight,
+    couponRightWidth,
+    couponBgImg,
+    pricePosition,
+    coupon,
+    btnText,
+    btnStryle,
+    isReceived,
+    receivedBtnText,
+    className,
+    ...rest
   } = {
     ...defaultProps,
     ...props,
-  }
-
+  };
+  const b = bem("biz-coupon");
+  useEffect(() => {}, []);
   return (
-    <div>
+    <div className={`${b()} ${className}`}>
+      <div
+        className={b("box")}
+        style={{
+          height: couponHeight,
+          width: couponWidth,
+          backgroundImage: `url(${couponBgImg})`,
+        }}
+      >
+        <div className={b("main")}></div>
+        <div className={b("btns")} style={{ width: couponRightWidth }}></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-Coupon.defaultProps = defaultProps
-Coupon.displayName = 'NutCoupon'
+Coupon.defaultProps = defaultProps;
+Coupon.displayName = "NutCoupon";
---
+++ src/packages/coupon/demo.tsx
@@ -0,0 +1,112 @@
+import React from "react";
+import { Coupon } from "./coupon";
+import { useTranslate } from "../../sites/assets/locale";
+
+interface T {
+  basic: string;
+  customProduct: string;
+  customPro1: string;
+  customPro2: string;
+  customPro3: string;
+  title: string;
+  customShop: string;
+  customPriceIcon: string;
+  customFooter: string;
+  customBottom: string;
+  customContent: string;
+  desc: string;
+  delivery: string;
+  shopName: string;
+}
+const CouponDemo = () => {
+  const [translated] = useTranslate<T>({
+    "zh-CN": {
+      basic: "基本用法",
+      customProduct: "自定义商品标签",
+      customPro1: "活鲜",
+      customPro2: "礼盒",
+      customPro3: "国产",
+      title:
+        "【活蟹】湖塘煙雨 阳澄湖大闸蟹公4.5两 母3.5两 4对8只 鲜活生鲜螃蟹现货水产礼盒海鲜水",
+      customShop: "自定义店铺介绍",
+      customPriceIcon: "价格后自定义标签",
+      customFooter: "自定义右下角内容",
+      customBottom: "自定义底部内容",
+      customContent: "自定义",
+      desc: "自营",
+      delivery: "厂商配送",
+      shopName: "阳澄湖大闸蟹自营店>",
+    },
+    "zh-TW": {
+      basic: "基本用法",
+      customProduct: "自定義商品標簽",
+      customPro1: "活鮮",
+      customPro2: "禮盒",
+      customPro3: "國產",
+      title:
+        "【活蟹】湖塘煙雨 陽澄湖大閘蟹公4.5兩 母3.5兩 4對8只 鮮活生鮮螃蟹現貨水產禮盒海鮮水",
+      customShop: "自定義店鋪介紹",
+      customPriceIcon: "價格後自定義標簽",
+      customFooter: "自定義右下角內容",
+      customBottom: "自定義底部內容",
+      customContent: "自定義",
+      desc: "自營",
+      delivery: "廠商配送",
+      shopName: "陽澄湖大閘蟹自營店>",
+    },
+    "en-US": {
+      basic: "Basic Usage",
+      customProduct: "Custom prolist",
+      customPro1: "tag",
+      customPro2: "tag",
+      customPro3: "tag",
+      title: "title",
+      customShop: "Custom Content",
+      customPriceIcon: "Price after custom tag",
+      customFooter: "Customize bottom right content",
+      customBottom: "Customize bottom content",
+      customContent: "custom",
+      desc: "desc",
+      delivery: "delivery",
+      shopName: "shopName>",
+    },
+  });
+  const state = {
+    imgUrl:
+      "//img10.360buyimg.com/n2/s240x240_jfs/t1/210890/22/4728/163829/6163a590Eb7c6f4b5/6390526d49791cb9.jpg!q70.jpg",
+    title: translated.title,
+    price: "388",
+    vipPrice: "378",
+    shopDesc: translated.desc,
+    delivery: translated.delivery,
+    shopName: translated.shopName,
+  };
+  const tagStyles = {
+    display: "inline-block",
+    verticalAlign: "middle",
+    marginRight: "5px",
+    marginLeft: "2px",
+    width: "29px",
+    height: "14px",
+  };
+  const wordStyles = {
+    padding: "0 5px",
+    borderRadius: "1px",
+    fontSize: "10px",
+    height: "15px",
+    lineHeight: "15px",
+    color: "#999",
+    backgroundColor: "#f2f2f7",
+    marginRight: "5px",
+  };
+  return (
+    <>
+      <div className="demo">
+        <h2>{translated.basic}</h2>
+        <Coupon />
+      </div>
+    </>
+  );
+};
+
+export default CouponDemo;
