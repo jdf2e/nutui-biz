c45b464da37d7a63faf0b2ab40109af7629557e4
songchenglin3 <songchenglin1@jd.com>
2023-02-03 11:06:03 +0800

    feat: ecard组件部分功能开发

---
+++ src/packages/ecard/demo.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { useTranslate } from "../../sites/assets/locale";
+import { Ecard } from "./ecard";
+import { Cell } from "@nutui/nutui-react";
+
+interface IDataList {
+  price: number;
+}
+interface T {
+  [props: string]: string;
+}
+
+const EcardDemo = () => {
+  const [translated] = useTranslate<T>({
+    "zh-CN": {
+      basic: "基本用法",
+    },
+    "zh-TW": {
+      basic: "基本用法",
+    },
+    "en-US": {
+      basic: "Basic Usage",
+    },
+  });
+  const [money, setMoney] = useState(10);
+  const [dataList] = useState<IDataList[]>([
+    {
+      price: 10,
+    },
+    {
+      price: 20,
+    },
+    {
+      price: 30,
+    },
+    {
+      price: 40,
+    },
+  ]);
+  const onChangeInput = (val: number) => {
+    setMoney(val);
+  };
+  const onChange = (item: { price: number }) => {
+    setMoney(item.price);
+  };
+  const onChangeStep = (num: number, price: number) => {
+    setMoney(price * num);
+  };
+  return (
+    <>
+      <div className="demo">
+        <h2>{translated.basic}</h2>
+        <Cell>
+          <Ecard
+            money={money}
+            onChangeInput={onChangeInput}
+            // onChange={onChange}
+            onChangeStep={onChangeStep}
+            dataList={dataList}
+          ></Ecard>
+        </Cell>
+      </div>
+    </>
+  );
+};
+
+export default EcardDemo;
