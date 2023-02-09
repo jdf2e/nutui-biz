3f1081fd5fe28dad59b3d101e347e7c51756c41c
yangxiaolu3 <yangxiaolu3@jd.com>
2023-02-09 13:55:17 +0800

    feat: category添加单元测试

--- src/packages/category/__test__/category.spec.tsx
+++ src/packages/category/__test__/category.spec.tsx
@@ -3,6 +3,7 @@ import { render, waitFor, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import { Category } from '../category'
 import { categoryInfo } from '../data'
+import { sleep } from '@/utils/test/event'
 
 const intersectionObserverMock = () => ({
   observe: () => null
@@ -42,6 +43,47 @@ test('Show quick nav',() => {
 
   const panel = container.querySelector('.nut-category-pane__quick-box')
   expect(panel).toBeInTheDocument()
+
+})
+
+test('Scroll quick nav',() => {
+  const { container } = render(
+    <Category category={categoryInfo}  showSecondLevelQuickNav={true}/>
+  )
+
+  const bodyEl = document.querySelectorAll(`.nut-category-pane__cate-list-right`)[0]
+  const elements = document.querySelectorAll(`.nut-category-pane__child-anchor`)
+
+  Object.defineProperty(bodyEl, 'scrollTop', {
+    value: 400,
+  })
+
+  elements.forEach((panel, i) =>
+    Object.defineProperties(panel, {
+      clientHeight: {
+        value: 260,
+      },
+      offsetTop: {
+        value: i * 260,
+      },
+    })
+  )
+
+  fireEvent.scroll(bodyEl)
+
+  const quickAction = document.querySelector('.nut-category-pane__quick-child-active')
+
+  expect(quickAction).toHaveTextContent('休闲食品')
+})
+
+
+test('Image lazy',async () => {
+  const { container } = render(
+    <Category category={categoryInfo}  showSecondLevelQuickNav={true}/>
+  )
+  const loading = container.querySelector('.nut-img-loading')
+
+  expect(loading).toBeInTheDocument()
 
 })
 
