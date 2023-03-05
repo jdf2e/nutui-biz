86de5ae5f53d4cb5f5377a0184571166c0043758
libin0824 <webcoderlb@163.com>
2023-03-05 21:05:54 +0800

    chore: fix ecard jest。

--- src/packages/ecard/__test__/ecard.spec.tsx
+++ src/packages/ecard/__test__/ecard.spec.tsx
@@ -21,54 +21,54 @@ const dataList = [
 ];
 
 test("should render correct modelValue", () => {
-  const { container } = render(<Ecard modelValue={0} dataList={dataList}></Ecard>);
-  const itemDoms = container.querySelectorAll(".nut-ecard__list__item");
+  const { container } = render(<Ecard dataList={dataList}></Ecard>);
+  const itemDoms = container.querySelectorAll(".nb-ecard__list__item");
   fireEvent.click(itemDoms[0]);
   expect(
-    container.querySelector(".nut-ecard__list__step--price")?.innerHTML
+    container.querySelector(".nb-ecard__list__step--price")?.innerHTML
   ).toBe("¥10");
 });
 
 test("input change when more than maxValue", () => {
   const { container } = render(
-    <Ecard modelValue={0} dataList={dataList} cardAmountMax={100}></Ecard>
+    <Ecard dataList={dataList} cardAmountMax={100}></Ecard>
   );
 
-  const inputDom = container.querySelector(".nut-ecard__list__input--input");
+  const inputDom = container.querySelector(".nb-ecard__list__input--input");
   if (inputDom) {
     fireEvent.change(inputDom, { target: { value: 123 } });
     fireEvent.click(inputDom);
   }
   expect(
-    container.querySelector(".nut-ecard__list__step--price")?.innerHTML
+    container.querySelector(".nb-ecard__list__step--price")?.innerHTML
   ).toBe("¥1000");
 });
 
 test("input change when less than maxValue", () => {
   const { container } = render(
-    <Ecard modelValue={0} dataList={dataList} cardAmountMin={100}></Ecard>
+    <Ecard dataList={dataList} cardAmountMin={100}></Ecard>
   );
-  const inputDom = container.querySelector(".nut-ecard__list__input--input");
+  const inputDom = container.querySelector(".nb-ecard__list__input--input");
   if (inputDom) {
     fireEvent.change(inputDom, { target: { value: 90 } });
     fireEvent.click(inputDom);
   }
   expect(
-    container.querySelector(".nut-ecard__list__step--price")?.innerHTML
+    container.querySelector(".nb-ecard__list__step--price")?.innerHTML
   ).toBe("¥100");
 });
 
 test("input change when less than maxValue", () => {
   const { container } = render(
-    <Ecard modelValue={10} dataList={dataList} cardAmountMin={100}></Ecard>
+    <Ecard dataList={dataList} cardAmountMin={100}></Ecard>
   );
-  const inputDom = container.querySelector(".nut-ecard__list__input--input");
+  const inputDom = container.querySelector(".nb-ecard__list__input--input");
   const addDom = container.querySelector(".nut-icon-plus");
   if (inputDom && addDom) {
     fireEvent.click(inputDom);
     fireEvent.click(addDom);
   }
   expect(
-    container.querySelector(".nut-ecard__list__step--price")?.innerHTML
+    container.querySelector(".nb-ecard__list__step--price")?.innerHTML
   ).toBe("¥");
 });
