d41c2a7289462f690fc023cd3e133c80894bca1a
zhenyulei <zhenyulei@jd.com>
2023-03-13 11:24:19 +0800

    fix: update coupon test

--- src/packages/coupon/__test__/coupon.spec.tsx
+++ src/packages/coupon/__test__/coupon.spec.tsx
@@ -114,13 +114,12 @@ test("prop of Basic Usage", () => {
   );
   //价格和单位的前后位置
   expect(
-    container.querySelectorAll(
-      ".nut-biz-coupon .nut-biz-coupon__main-price div"
-    )[1]?.innerHTML
+    container.querySelectorAll(".nb-coupon .nb-coupon__main-price div")[1]
+      ?.innerHTML
   ).toBe("¥");
   //点击按钮事件
   const nutCoupon = container.querySelector(
-    ".nut-biz-coupon .nut-biz-coupon__btns .nut-button--primary"
+    ".nb-coupon .nb-coupon__btns .nut-button--primary"
   ) as HTMLElement;
   fireEvent.click(nutCoupon);
   expect(basedOnClick).toBeCalled();
@@ -182,6 +181,126 @@ test("small type of coupon", () => {
   );
 
   expect(
-    container.querySelector(".nut-biz-coupon .nut-biz-coupon__btns div")
-  ).toHaveClass("nut-biz-coupon__btns-vertcal");
+    container.querySelector(".nb-coupon .nb-coupon__btns div")
+  ).toHaveClass("nb-coupon__btns-vertcal");
+});
+
+test("test price length", () => {
+  const basedOnClick = jest.fn();
+  const buttonProps: Partial<ButtonPropsType> = {
+    type: "primary",
+    size: "small",
+    plain: true,
+    className: "cancel-btn",
+  };
+  const state = {
+    //优惠券样式
+    couponBaseStyle: {
+      width: "100%",
+      height: "auto",
+      backgroundImage: `url(https://storage.360buyimg.com/jdcdkh/open/1.0.0/assets/bg-coupon-red.f6ae2e19.png)`,
+    },
+    couponMainBaseStyle: {
+      width: "69%",
+      color: "#fff",
+    },
+    baseCouponObj: {
+      price: "9.123",
+      currency: "¥",
+      mainTitle: "满100元可用",
+      subTitle: "仅可购买满折券测试",
+      label: "内购专享",
+      timeRange: "2022.03.01-2022.04.01",
+    },
+    btnText: "立即领取",
+    receivedStatus: false,
+    usedIcon: (
+      <img
+        src="https://storage.360buyimg.com/jdcdkh/open/1.0.0/assets/use-mask.60dc7c10.png"
+        width="45px"
+        height="42px"
+      />
+    ),
+    buttonProps: buttonProps,
+  };
+  const { container } = render(
+    <>
+      <Coupon
+        pricePosition="front"
+        couponStyle={state.couponBaseStyle}
+        couponMainStyle={state.couponMainBaseStyle}
+        couponData={state.baseCouponObj}
+        btnText={state.btnText}
+        isReceived={state.receivedStatus}
+        usedIcon={state.usedIcon}
+        buttonProps={state.buttonProps}
+        onBtnClick={basedOnClick}
+      ></Coupon>
+    </>
+  );
+  //价格和单位的前后位置
+  expect(
+    container.querySelector(".nb-coupon .nb-coupon__main-price--number")
+      ?.textContent
+  ).toBe("9.12");
+});
+
+test("test price length sec", () => {
+  const basedOnClick = jest.fn();
+  const buttonProps: Partial<ButtonPropsType> = {
+    type: "primary",
+    size: "small",
+    plain: true,
+    className: "cancel-btn",
+  };
+  const state = {
+    //优惠券样式
+    couponBaseStyle: {
+      width: "100%",
+      height: "auto",
+      backgroundImage: `url(https://storage.360buyimg.com/jdcdkh/open/1.0.0/assets/bg-coupon-red.f6ae2e19.png)`,
+    },
+    couponMainBaseStyle: {
+      width: "69%",
+      color: "#fff",
+    },
+    baseCouponObj: {
+      price: "9.1",
+      currency: "¥",
+      mainTitle: "满100元可用",
+      subTitle: "仅可购买满折券测试",
+      label: "内购专享",
+      timeRange: "2022.03.01-2022.04.01",
+    },
+    btnText: "立即领取",
+    receivedStatus: false,
+    usedIcon: (
+      <img
+        src="https://storage.360buyimg.com/jdcdkh/open/1.0.0/assets/use-mask.60dc7c10.png"
+        width="45px"
+        height="42px"
+      />
+    ),
+    buttonProps: buttonProps,
+  };
+  const { container } = render(
+    <>
+      <Coupon
+        pricePosition="front"
+        couponStyle={state.couponBaseStyle}
+        couponMainStyle={state.couponMainBaseStyle}
+        couponData={state.baseCouponObj}
+        btnText={state.btnText}
+        isReceived={state.receivedStatus}
+        usedIcon={state.usedIcon}
+        buttonProps={state.buttonProps}
+        onBtnClick={basedOnClick}
+      ></Coupon>
+    </>
+  );
+  //价格和单位的前后位置
+  expect(
+    container.querySelector(".nb-coupon .nb-coupon__main-price--number")
+      ?.textContent
+  ).toBe("9.1");
 });
